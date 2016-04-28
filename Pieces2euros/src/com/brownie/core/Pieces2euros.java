6395a2694aed058ac9eed384889e9e2706b1a80c
Your Name <you@example.com>
2016-04-28 14:53:41 +0200

    Add sqlite usage.

--- Pieces2euros/src/com/brownie/core/Pieces2euros.java
+++ Pieces2euros/src/com/brownie/core/Pieces2euros.java
@@ -1,9 +1,28 @@
 package com.brownie.core;
 
+import org.apache.log4j.LogManager;
+import org.apache.log4j.Logger;
+
+import com.brownie.db.SQLiteJDBC;
+
 public class Pieces2euros {
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+	private static final Logger logger = LogManager
+			.getLogger(Pieces2euros.class);
+
+	public Pieces2euros() throws Exception {
+
+		SQLiteJDBC WebStore = new SQLiteJDBC();
+//		WebStore.createDB();
+//		WebStore.populateDB();
+		WebStore.showItems();
+		logger.info("Test");
+
+	}
+
+	public static void main(String[] args) throws Exception {
+
+		new Pieces2euros();
 
 	}
 
